b38d866c801ca5f7b1b1870d8305f0bd7f042473
Antoine Boileau <apem.boileau@gmail.com>
2016-11-17 09:28:59 +0100

    Initial Commit

---
+++ Compass/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Compass
+//
+//  Created by Antoine Boileau on 17/11/16.
+//  Copyright © 2016 Antoine Boileau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
