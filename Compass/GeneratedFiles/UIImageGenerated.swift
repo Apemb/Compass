741ee2cf4f2f639fafcd91b4ea68c62f014f9f3f
Antoine Boileau <apem.boileau@gmail.com>
2016-11-26 00:19:04 +0100

    [splash_screen] Added swiftgen Images

---
+++ Compass/GeneratedFiles/UIImageGenerated.swift
@@ -0,0 +1,30 @@
+// Generated using SwiftGen, by O.Halligon — https://github.com/AliSoftware/SwiftGen
+// Custom template by OCTO - putting images in camelCase when written like IC_IMAGE
+
+#if os(iOS) || os(tvOS) || os(watchOS)
+  import UIKit.UIImage
+  typealias Image = UIImage
+#elseif os(OSX)
+  import AppKit.NSImage
+  typealias Image = NSImage
+#endif
+
+// swiftlint:disable file_length
+// swiftlint:disable line_length
+
+// swiftlint:disable type_body_length
+enum Asset: String {
+  case compassSplashIcon = "Compass_Splash_Icon"
+
+  var image: Image {
+    return Image(asset: self)
+  }
+}
+// swiftlint:enable type_body_length
+
+extension Image {
+  convenience init!(asset: Asset) {
+    self.init(named: asset.rawValue)
+  }
+}
+
